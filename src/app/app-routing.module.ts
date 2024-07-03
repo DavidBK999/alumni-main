2ea379c509d07cb899802254f1944081e794caff
DavidBK999 <david.koeberl@gmx.at>
2024-07-03 13:11:02 +0200

    added sebastian comp. and zelimchan comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CareerComponent } from './career/career.component';
+import { Career2Component } from './career2/career2.component';
 import { HomeComponent } from './home/home.component';
 import { AlumniComponent } from './alumni/alumni.component';
 import { Alumni1Component } from './alumni1/alumni1.component';
 import { Alumni2Component } from './alumni2/alumni2.component';
+import { EventsComponent } from './events/events.component';
 
 const routes: Routes = [
+  { path: 'career', component: CareerComponent },
   {
     path: 'home',
     component: HomeComponent,
@@ -23,10 +27,9 @@ const routes: Routes = [
     path: 'alumni2',
     component: Alumni2Component,
   },
-  {
-    path: `**`,
-    redirectTo: `home`,
-  },
+  { path: 'career2', component: Career2Component },
+  { path: 'events', component: EventsComponent },
+  { path: '**', redirectTo: 'career', pathMatch: 'full' },
 ];
 
 @NgModule({
