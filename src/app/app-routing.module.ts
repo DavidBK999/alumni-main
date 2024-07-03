f2ddd59d3c2f8dd09ab409fc6218bcc6f1b9f955
DavidBK999 <david.koeberl@gmx.at>
2024-07-03 11:12:31 +0200

    Finished Alumni parts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AlumniComponent } from './alumni/alumni.component';
+import { Alumni1Component } from './alumni1/alumni1.component';
+import { Alumni2Component } from './alumni2/alumni2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'alumni',
+    component: AlumniComponent,
+  },
+  {
+    path: 'alumni1',
+    component: Alumni1Component,
+  },
+  {
+    path: 'alumni2',
+    component: Alumni2Component,
+  },
+  {
+    path: `**`,
+    redirectTo: `home`,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
